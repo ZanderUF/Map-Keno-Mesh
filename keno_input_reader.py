4b577a308047e9fba2f3cb31131841df071f6286
Alexander J. Mausolff <alexander.mausolff@inl.gov>
2016-07-07 16:36:51 -0600

    writes to one file the fission den, power den, and corresponding unit number

--- keno_input_reader.py
+++ keno_input_reader.py
@@ -8,12 +8,11 @@ kenoFileInput = open(kenoFile, 'r+')
 outputFiss = open('outputFissTemp','w')
 outputUnit = open('outputUnitFinal','w')
 outputUnitArray = []
-outputFissArray = []
+#-----Parse keno input to find the fission density and corresponding unit number
 for line in kenoFileInput:
         if "              unit      region   density     deviation    productions          density     deviation      fissions" in line:
                 for line in kenoFileInput:
                         outputFiss.write(line[33:42] + '\n')
-                        outputFissArray.append(line[33:42])
                         if "  global unit" in line: 
                                 break        
                         if line[15:18].isspace():
@@ -29,16 +28,25 @@ outputFiss.flush()
 inputFiss = open('outputFissTemp', 'r+').readlines()
 #output = open('outputFissFinal','w')
 
+#-----Add all the fission densities per region -> total  fission density per unit
+#-----Fission density [fissions/cm^3]
+#-----Multiple fission density by "kappa" fission/energhy
+#-----Kappa value is 3.1E-11 Joules/fission, from 192MeV/fission for U235
 inputF = inputFiss[1:-1]
 total = 0.0
 inputFarray = []
+powerDenArray = []
+#kappa = 0.000000000031
+kappa = 3.1E-11
 for line in inputF:
         if line.isspace():
+                powerDenArray.append( "%.18f" % (total * kappa))
+                total = "%.11f" % total
                 inputFarray.append(total)
                 total = 0.0
         else:
                 total = total + float(line)
 
-output = '\n'.join('\t'.join(map(str,row)) for row in zip(inputFarray,outputUnitArray))
+output = '\n'.join('\t'.join(map(str,row)) for row in zip(inputFarray,powerDenArray,outputUnitArray))
 with open('outputUnitFissFinal', 'w') as f:
     f.write(output)
